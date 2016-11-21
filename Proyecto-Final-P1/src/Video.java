5939fa9d24a3c421f9aea59bba90fe5a5f26460c
samirant15 <samirant15@gmail.com>
2016-11-21 13:26:03 -0400

    Add files via upload

---
+++ Proyecto-Final-P1/src/Video.java
@@ -0,0 +1,22 @@
+
+public class Video {
+
+	private int resX, resY; //resolucion
+
+	public int getResX() {
+		return resX;
+	}
+
+	public void setResX(int resX) {
+		this.resX = resX;
+	}
+
+	public int getResY() {
+		return resY;
+	}
+
+	public void setResY(int resY) {
+		this.resY = resY;
+	}
+	
+}
