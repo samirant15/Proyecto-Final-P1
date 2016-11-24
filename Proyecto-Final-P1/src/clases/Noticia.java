f88016ea28bfdc1eb39c2e4957a556d177e6afaa
samir <samir@samir-pc>
2016-11-24 19:29:28 -0400

    Modifique la clase noticia

--- Proyecto-Final-P1/src/clases/Noticia.java
+++ Proyecto-Final-P1/src/clases/Noticia.java
@@ -2,7 +2,34 @@ package clases;
 //De: Samir y Luilli
 public class Noticia {
 	
+	private String titulo;
 	private String texto;
+	private int fecha;
+	private Usuario user;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(int fecha) {
+		this.fecha = fecha;
+	}
+
+	public Usuario getUser() {
+		return user;
+	}
+
+	public void setUser(Usuario user) {
+		this.user = user;
+	}
 
 	public String getTexto() {
 		return texto;
