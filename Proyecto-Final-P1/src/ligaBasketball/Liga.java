95eb519fe2942ce99b71f11e1dfa7e99ee476a10
karvinXD <karvinjimenezxd@gmail.com>
2016-11-29 18:03:27 -0400

    Update Liga.java
    Actualice clase liga

--- Proyecto-Final-P1/src/ligaBasketball/Liga.java
+++ Proyecto-Final-P1/src/ligaBasketball/Liga.java
@@ -2,22 +2,67 @@ package ligaBasketball;
 
 import java.util.ArrayList;
 
+
 public class Liga {
 
-	private static Liga LIGA = null;
-	private Liga(){};
+    private static Liga Liga = null;
+	private Liga(){}
 	public static Liga getInstance(){
-		if(LIGA == null)
-			LIGA = new Liga();
-		return LIGA;
-		
+		if(Liga == null)
+			Liga = new Liga();
+		return Liga;
+	}
+
+	private ArrayList<Partido> partidos = new ArrayList<Partido>();
+	private ArrayList<Equipo> equipos = new ArrayList<Equipo>();
+	private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+	
+	public static Liga getLiga() {
+		return Liga;
 	}
 	
-	ArrayList<Equipo> equipos = new ArrayList<Equipo>();
+	public static void setLiga(Liga liga) {
+		Liga = liga;
+	}
+	
+	public ArrayList<Partido> getPartidos() {
+		return partidos;
+	}
+	
+	public void setPartidos(ArrayList<Partido> partidos) {
+		this.partidos = partidos;
+	}
+	
+	public ArrayList<Equipo> getEquipos() {
+		return equipos;
+	}
+	
+	public void setEquipos(ArrayList<Equipo> equipos) {
+		this.equipos = equipos;
+	}
+	
+	
+	
+	public void InsertarJugador(Jugador jugador)
+	{
+		jugadores.add(jugador);
+	}
+	
+	
+	public void InsertarEquipo(Equipo equipo)
+	{
+		equipos.add(equipo);
+	}
+	
+	public void InsertarPartidos(Partido partido)
+	{
+		partidos.add(partido);
+	}
 	
 	
 	
 }
+
 	
 	
 
