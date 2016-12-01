60b8aff2d462ac4d0d5d93d9ad8193ee9f6278f1
Juan Thomas <Juan Thomas@JuanThomasAngel>
2016-12-01 12:03:24 -0400

    Hilo del foro

---
+++ Proyecto-Final-P1/src/onlineMedia/Hilo.java
@@ -0,0 +1,21 @@
+package onlineMedia;
+
+public class Hilo {
+	private Usuario usuario = new Usuario();
+	private String mensajes;
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public String getMensajes() {
+		return mensajes;
+	}
+	public void setMensajes(String mensajes) {
+		this.mensajes = mensajes;
+	}
+	
+	
+}
