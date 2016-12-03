7e1706a778bcee2f06b5f474be3ea0f34631e631
samir <samir@samir-pc>
2016-12-03 17:28:38 -0400

    Agregue unas clases y la libreria de vlc (90mb)

---
+++ Proyecto-Final-P1/src/onlineMedia/ConectorArchivoNoticia.java
@@ -0,0 +1,109 @@
+package onlineMedia;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+
+public class ConectorArchivoNoticia {
+	private static Socket sock;
+    private static String fileName;
+    private static BufferedReader stdin;
+    private static PrintStream os;
+
+    public void Crear() throws IOException {
+        try {
+        	System.out.println("Client started.");
+            sock = new Socket("localhost", 5678);
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+        } catch (Exception e) {
+            System.err.println("Cannot connect to the server, try again later.");
+            System.exit(1);
+        }
+
+        os = new PrintStream(sock.getOutputStream());
+
+        try {
+                os.println("Recive");
+                System.err.print("Enter file name: ");
+                fileName = stdin.readLine();
+                os.println(fileName);
+                receiveFile(fileName);
+        } catch (Exception e) {
+            System.err.println("not valid input");
+        }
+
+
+        sock.close();
+    }
+
+
+
+    public static void sendFile() {
+        try {
+            System.err.print("Enter file name: ");
+            fileName = stdin.readLine();
+
+            File myFile = new File(fileName);
+            byte[] mybytearray = new byte[(int) myFile.length()];
+
+            FileInputStream fis = new FileInputStream(myFile);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            //bis.read(mybytearray, 0, mybytearray.length);
+
+            DataInputStream dis = new DataInputStream(bis);
+            dis.readFully(mybytearray, 0, mybytearray.length);
+
+            OutputStream os = sock.getOutputStream();
+
+            //Sending file name and file size to the server
+            DataOutputStream dos = new DataOutputStream(os);
+            dos.writeUTF(myFile.getName());
+            dos.writeLong(mybytearray.length);
+            dos.write(mybytearray, 0, mybytearray.length);
+            dos.flush();
+            System.out.println("File "+fileName+" sent to Server.");
+        } catch (Exception e) {
+            System.err.println("File does not exist!");
+        }
+    }
+
+    public static void receiveFile(String fileName) {
+        try {
+            int bytesRead;
+            InputStream in = sock.getInputStream();
+
+            DataInputStream clientData = new DataInputStream(in);
+
+            fileName = clientData.readUTF();
+            OutputStream output = new FileOutputStream(("received_from_server_" + fileName));
+            long size = clientData.readLong();
+            byte[] buffer = new byte[1024];
+            while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+                output.write(buffer, 0, bytesRead);
+                size -= bytesRead;
+            }
+
+            output.close();
+            in.close();
+
+            System.out.println("File "+fileName+" received from Server.");
+        } catch (IOException ex) {
+            Logger.getLogger(ConectorClienteNoticia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ Proyecto-Final-P1/src/onlineMedia/ServidorNoticia.java
@@ -0,0 +1,40 @@
+package onlineMedia;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorNoticia  implements Runnable {
+	private static ServerSocket serverSocket;
+    private static Socket clientSocket = null;
+    
+    public ServidorNoticia() {
+        
+    }
+    
+    public void run() {
+
+        try {
+            serverSocket = new ServerSocket(5678);
+            System.out.println("Server started.");
+        } catch (Exception e) {
+            System.err.println("Port already in use.");
+            System.exit(1);
+        }
+
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection : " + clientSocket);
+
+                Thread t = new Thread(new ConectorClienteNoticia(clientSocket));
+
+                t.start();
+
+            } catch (Exception e) {
+                System.err.println("Error in connection attempt.");
+            }
+        }
+    }
+}
+
