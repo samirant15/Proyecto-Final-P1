f8712db602c52b961603a6d5cc9adf3d22610719
Enmanuel07 <enmanuel.bueno@gmail.com>
2016-12-05 14:49:36 -0400

    hjh

--- Proyecto-Final-P1/src/clases/Inicio.java
+++ Proyecto-Final-P1/src/clases/Inicio.java
@@ -1,9 +1,15 @@
 package clases;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import enums.Posicion;
 import enums.Roles;
 import ligaBasketball.Liga;
+import ligaBasketball.JugadorFantasy;
 import ui.LoginJDialog;
 
 public class Inicio {
@@ -11,6 +17,7 @@ public class Inicio {
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		// TODO Auto-generated method stub
 		
+
 	    Liga.getInstance().imprimirUsuarios();
 	    Liga.getInstance().leerBinario();
         LoginJDialog login = new LoginJDialog();
